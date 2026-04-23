8ba05c66e974ad89cf1fd198a74c7269f7b61c37
H6B1 <et.jeden@gmail.com>
2026-04-23 18:58:35 +0200

    template for linq

---
+++ 26/lekcjaLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Student
+{
+    public string Imie { get; set; }
+    public int Wiek { get; set; }
+    public float Srednia { get; set; }
+}
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+
+
+// -- DATA -- 
+// Imie = "Anna", Wiek = 20, Srednia = 4.65f
+// Imie = "Zbysiu", Wiek = 22, Srednia = 5.12f
+// Imie = "Marta", Wiek = 19, Srednia = 4.87f
+// Imie = "Karolina", Wiek = 23, Srednia = 3.89f
