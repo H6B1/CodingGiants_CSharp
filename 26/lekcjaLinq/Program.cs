fd5e071226fba987bc902094567804cde717348c
H6B1 <et.jeden@gmail.com>
2026-04-23 19:15:10 +0200

    template for linq

--- 26/lekcjaLinq/Program.cs
+++ 26/lekcjaLinq/Program.cs
@@ -8,7 +8,15 @@ public class Program
 {
     public static void Main()
     {
+        var studenci = new List<Student>
+        {
+            new Student { Imie = "Anna", Wiek = 20, Srednia = 4.65f },
+            new Student { Imie = "Zbysiu", Wiek = 22, Srednia = 5.12f },
+            new Student { Imie = "Marta", Wiek = 19, Srednia = 4.87f },
+            new Student { Imie = "Karolina", Wiek = 23, Srednia = 3.89f }
+        };
 
+        
     }
 }
 
